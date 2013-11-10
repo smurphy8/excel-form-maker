c673d4eb9cedf7e06204f1c5c5b238a864db3694
Scott Murphy <scott@scott-VPCF226FM.(none)>
2013-11-09 21:31:01 -0600

    ran into problems with formatting

--- src/Data/Excel/FormPopulate.hs
+++ src/Data/Excel/FormPopulate.hs
@@ -1,10 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Data.Excel.FormPopulate where
 
 import Data.Excel.FormPopulate.Internal
 import Codec.Xlsx.Parser
+import Codec.Xlsx.Writer
+import Codec.Xlsx.Lens
+import Codec.Xlsx
+
+
+
+
+
+
+
 
 
 createForm :: IO () 
 createForm = do 
-  xlsx "DEQ_Template.xlsx"
-  print "Hello" 
+  x <- xlsx "BuildSheet.xlsx"
+  ws <- getWorksheets x
+  editWs <- return $ setMultiMappedSheetCellData ws [FICV 0 1 3 (CellText"TestValue")]
+  writeXlsx "ptest2.xlsx" x (Just editWs)
