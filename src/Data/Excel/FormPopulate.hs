2fb6f6d8496cfec8095e88322a8da288f27ad1c4
Scott Murphy <scott@scott-VPCF226FM.(none)>
2013-11-10 22:32:24 -0600

    adding Persist.MongoDB

--- src/Data/Excel/FormPopulate.hs
+++ src/Data/Excel/FormPopulate.hs
@@ -1,13 +1,40 @@
 {-# LANGUAGE OverloadedStrings #-}
-
+{-# LANGUAGE EmptyDataDecls    #-}
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE GADTs             #-}
+{-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TypeFamilies      #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 module Data.Excel.FormPopulate where
+import Yesod
+import qualified Database.Persist
+import Yesod.Default.Config (DefaultEnv (..), withYamlEnvironment)
 
+import Database.Persist 
+import Database.Persist.MongoDB
+import Database.Persist.TH
 import Data.Excel.FormPopulate.Internal
+import Data.IntMap
+import Data.Text
 import Codec.Xlsx.Parser
 import Codec.Xlsx.Writer
 import Codec.Xlsx.Lens
 import Codec.Xlsx
+import Language.Haskell.TH.Syntax
 
+
+
+dataList = [ FICV 0 16 11 (CellDouble 333.0), FICV 0 16 13 (CellDouble 332.0), FICV 0 16 13 (CellDouble 332.0), FICV 0 16 14 (CellDouble 332.0),
+             FICV 0 17 11 (CellDouble 333.0), FICV 0 17 13 (CellDouble 332.0), FICV 0 17 13 (CellDouble 332.0), FICV 0 17 14 (CellDouble 332.0),
+             FICV 0 18 11 (CellDouble 333.0), FICV 0 18 13 (CellDouble 332.0), FICV 0 18 13 (CellDouble 332.0), FICV 0 18 14 (CellDouble 332.0),
+             FICV 0 19 11 (CellDouble 333.0), FICV 0 19 13 (CellDouble 332.0), FICV 0 19 13 (CellDouble 332.0), FICV 0 19 14 (CellDouble 332.0),
+             FICV 0 20 11 (CellDouble 333.0), FICV 0 20 13 (CellDouble 332.0), FICV 0 20 13 (CellDouble 332.0), FICV 0 20 14 (CellDouble 332.0),
+             FICV 0 21 11 (CellDouble 333.0), FICV 0 21 13 (CellDouble 332.0), FICV 0 21 13 (CellDouble 332.0), FICV 0 21 14 (CellDouble 332.0),
+             FICV 0 22 11 (CellDouble 333.0), FICV 0 22 13 (CellDouble 332.0), FICV 0 22 13 (CellDouble 332.0), FICV 0 22 14 (CellDouble 332.0),
+             FICV 0 23 11 (CellDouble 333.0), FICV 0 23 13 (CellDouble 332.0), FICV 0 23 13 (CellDouble 332.0), FICV 0 23 14 (CellDouble 332.0),
+             FICV 0 24 11 (CellDouble 333.0), FICV 0 24 13 (CellDouble 332.0), FICV 0 24 13 (CellDouble 332.0), FICV 0 24 14 (CellDouble 332.0)]
+           
 
 
 
@@ -20,5 +47,26 @@ createForm :: IO ()
 createForm = do 
   x <- xlsx "BuildSheet.xlsx"
   ws <- getWorksheets x
-  editWs <- return $ setMultiMappedSheetCellData ws [FICV 0 1 3 (CellText"TestValue")]
+  editWs <- return $ setMultiMappedSheetCellData ws dataList
   writeXlsx "ptest2.xlsx" x (Just editWs)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,38 @@
-{-# LANGUAGE NoImplicitPrelude,OverloadedStrings #-}
-module Main where
+{-# LANGUAGE OverloadedStrings, TypeFamilies, QuasiQuotes,
+  TemplateHaskell, MultiParamTypeClasses, GADTs, FlexibleContexts
+, EmptyDataDecls #-}
+import Yesod
+import Yesod.Default.Config (DefaultEnv (..), withYamlEnvironment)
+import Data.Text (Text, pack)
+import Database.Persist (runPool, createPoolConfig, loadConfig,applyEnv)
+import Database.Persist.MongoDB (MongoConf (..), Action, ConnectionPool (..), MongoBackend)
+import Language.Haskell.TH.Syntax
+import Control.Applicative ((<$>), (<*>), liftA2, Applicative)
 
-import Prelude  (IO, print)
-
-import Data.Excel.FormPopulate
 
 
-main :: IO ()
-main = createForm
+
+
+share [mkPersist (mkPersistSettings (ConT ''MongoBackend)) { mpsGeneric = False }, mkMigrate "migrateAll"][persistLowerCase|
+Questionnaire
+  desc Text Maybe
+  questions [Question]
+  deriving Show Eq Read 
+Question
+  formulation Text
+  deriving Show Eq Read 
+|]
+
+{-===========================================================================-}
+{-                                 MAIN                                      -}
+{-===========================================================================-}
+
+
+main = do
+    -- dbconf <- withYamlEnvironment mongoConfFile Development
+    --             Database.Persist.loadConfig >>=
+    --           Database.Persist.applyEnv
+    -- pool <- Database.Persist.createPoolConfig (dbconf)
+    return () 
+--    warp 3000 $ App pool dbconf
+--    where mongoConfFile = "./mongoDB-nonscaffold.yml" 
